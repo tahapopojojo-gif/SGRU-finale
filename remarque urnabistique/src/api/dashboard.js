40e858947e9af4ce60ae0ad88f70de06dffc298b
yahya <yahyaprogrammation@gmail.com>
2026-05-11 00:19:07 +0100

    feat: connect frontend to Laravel backend

--- remarque urnabistique/src/api/dashboard.js
+++
@@ -1,9 +0,0 @@
-import api from './client';
-
-export const dashboardApi = {
-
-    getStats: async () => {
-        const response = await api.get('/dashboard/stats');
-        return response.data.data;
-    },
-};
